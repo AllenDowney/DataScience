07bb8a499ee5cd3eec2f6aa5d1d4a5295d75f3fe
paulruvolo <paullundyruvolo@gmail.com>
2014-04-11 12:44:45 -0400

    added materials for second ML lecture

--- learn_digits_logistic_regression.py
+++ learn_digits_logistic_regression.py
@@ -15,21 +15,24 @@ from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
 print data.DESCR
-n_trials = 1
+n_trials = 100
 train_percentages = range(5,95,5)
+num_train = []
 test_accuracies = numpy.zeros(len(train_percentages))
 
 for (i,train_percent) in enumerate(train_percentages):
-	test_accuracy = numpy.zeros(n_trials)
-	for n in range(n_trials):
-		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_percent/100.0)
-		model = LogisticRegression()
-		model.fit(X_train, y_train)
-		test_accuracy[n] = model.score(X_test, y_test)
-	test_accuracies[i] = test_accuracy.mean()
+    test_accuracy = numpy.zeros(n_trials)
+    num_train.append(0)
+    for n in range(n_trials):
+        X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_percent/100.0)
+        num_train[i] = len(y_train)
+        model = LogisticRegression(C=1)
+        model.fit(X_train, y_train)
+        test_accuracy[n] = model.score(X_test, y_test)
+    test_accuracies[i] = test_accuracy.mean()
 
 fig = plt.figure()
-plt.plot(train_percentages, test_accuracies)
-plt.xlabel('Percentage of Data Used for Training')
+plt.plot(num_train, test_accuracies)
+plt.xlabel('Number of Training Instances')
 plt.ylabel('Accuracy on Test Set')
 plt.show()
